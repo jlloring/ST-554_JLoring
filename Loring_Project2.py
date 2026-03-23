8dd86de4bc8adfd18ad8445ec550f90e28a532e7
jlloring <84785772+jlloring@users.noreply.github.com>
2026-03-23 11:48:02 -0400

    Finished first & second validation method

--- Loring_Project2.py
+++ Loring_Project2.py
@@ -45,7 +45,7 @@ class SparkDataCheck:
         return cls(df)
     
     #creates interval validation method
-    def interval_chk(self, col: str, lower: str = None, upper: str = None):
+    def interval_chk(self, col_name: str, lower: str = None, upper: str = None):
         
         #checks that at least one bound is provided
         if lower is None and upper is None:
@@ -53,13 +53,13 @@ class SparkDataCheck:
             return self #returns itself
         
         #checks that column supplied is in the dataframe
-        if col not in self.df.columns:
+        if col_name not in self.df.columns:
             print("The column you supplied does not exist in the dataframe. Please try again.")
             return self #returns itself
         
         #grab the dtypes for each column using the dtype attribute and passing to a dictionary
         dtypes_dict = dict(self.df.dtypes)
-        col_type = dtypes_dict[col] #stores the type of the column that the user supplies into the method
+        col_type = dtypes_dict[col_name] #stores the type of the column that the user supplies into the method
         
         #check if the user supplied a non-numeric column
         if col_type not in ("float", "int", "longint", "bigint", "double", "integer"):
@@ -67,8 +67,37 @@ class SparkDataCheck:
             return self #returns itself
         
         #return True/False based on whether or not column value is in range supplied, account for NULL
+        ## build condition checker for supplying into withColumn
+        if lower is not None and upper is not None:
+            cond = col(col_name).between(lower, upper) #condition when both bounds are supplied
+        elif lower is not None:
+            cond = col(col_name) >= lower #condition when only lower bound is supplied
+        else:
+            cond = col(col_name) <= upper #condition when only upper bound is supplied
+            
+        ## create Interval_Check column and account for NULL values
+        self.df = self.df.withColumn("Interval_Check", when(col(col_name).isNull(), None).otherwise(cond))
+        return self #returns itself
+    
+    #creates levels validation method
+    def levels_chk(self, col_name: str, levels):
         
+        #checks that column supplied is in the dataframe
+        if col_name not in self.df.columns:
+            print("The column you supplied does not exist in the dataframe. Please try again.")
+            return self #returns itself
         
+        #grab the dtypes for each column using the dtype attribute and passing to a dictionary
+        dtypes_dict = dict(self.df.dtypes)
+        col_type = dtypes_dict[col_name] #stores the type of the column that the user supplies into the method
         
-        
-        
+        #check if the user supplied a non-string column
+        if col_type not in ("string"):
+            print ("The column you supplied is of non-string type. Please try again.")
+            return self #returns itself
+            
+        # create Levels_Check column and account for NULL values
+        self.df = self.df.withColumn("Levels_Check", when(col(col_name).isNull(), None).otherwise(col(col_name).isin(levels)))
+        return self #returns itself
+    
+    
